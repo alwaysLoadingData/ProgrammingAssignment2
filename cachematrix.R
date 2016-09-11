eb79069d0cf125a277bafe1a9403998ba280ce46
AlwaysLoadingData <AlwaysLoadingData@gmail.com>
2016-09-11 18:16:39 -0500

    R Programming Week 2 assignment.
    Implement cachematrix.R functions.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,69 @@
-## Put comments here that give an overall description of what your
-## functions do
+## CacheMatrix.R contains functions to work with a matrix
+## This file adds functionality to be able to cache the value of the
+## inverse of the matrix, so that if the value hasn't changed, the 
+## cached value will be used instead of re-solving the matrix.
 
-## Write a short comment describing this function
+
+## makeCacheMatrix contains functions for creating and modifying
+## the base matrix values
+
+## usage:
+## Create matrix list object
+## mtx <- makeCacheMatrix(initial_value)
+
+## Update matrix value
+## mtx$set(new_value)
+
+## Get matrix value
+## mtx$get()
 
 makeCacheMatrix <- function(x = matrix()) {
+
+	cached_inverse <- NULL
+	
+	get <- function() {
+		x
+	}
+	
+	set <- function(new_value) {
+		cached_inverse <<- NULL
+		x <<- new_value
+	}
+	
+	get_inverse <- function() {
+		cached_inverse
+	}
+	
+	set_inverse <- function(inverse) {
+		cached_inverse <<- inverse
+	}
+	
+	list(get = get, set = set,
+		 get_inverse = get_inverse, set_inverse = set_inverse)
 
 }
 
 
-## Write a short comment describing this function
+## cacheSolve returns the inverse of the base matrix.
+## if the matrix has been previously solved, then 
+## the cached value is used.
+
+## Usage:
+## cacheSolve(mtx)
+## mtx - was created by makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	inverse <- x$get_inverse()
+
+	if(!is.null(inverse))
+	{
+		message("returning cached data...")
+		return(inverse)
+	}
+	
+	inverse <- solve(x$get(), ...)
+	x$set_inverse(inverse)
+	
+	## Return a matrix that is the inverse of 'x'
+	inverse
 }
